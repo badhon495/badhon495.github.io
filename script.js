3605408d56c59a025dbfb4a3cca2de36047defb6
Badhon <badhon495@gmail.com>
2023-12-29 18:37:54 +0600

    fixed dark mode

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
+// Check if the browser prefers dark mode
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    body.style.background = '#1D2023';
+    body.style.color = 'white';
+    toggle.classList.remove('bi-brightness-high-fill');
+    toggle.classList.add('bi-moon');
+} else {
+    body.style.background = 'white';
+    body.style.color = 'black';
+    toggle.classList.remove('bi-moon');
+    toggle.classList.add('bi-brightness-high-fill');
+}
+
 toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = 'white';
-        body.style.color = 'black';
-        body.style.transition = '2s';
-    }else{
+    if(body.style.background == 'white'){
         body.style.background = '#1D2023';
         body.style.color = 'white';
         body.style.transition = '2s';
+        toggle.classList.remove('bi-brightness-high-fill');
+        toggle.classList.add('bi-moon');
+    }else{
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+        toggle.classList.remove('bi-moon');
+        toggle.classList.add('bi-brightness-high-fill');
     }
 });
